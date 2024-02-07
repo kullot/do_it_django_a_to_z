291fc89584ea3e99e8a08e565275f33f9fce5420
wlsdn123 <ness9497@gmail.com>
2024-02-08 01:00:55 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,9 @@ def index(request):
     posts = Post.objects.all().order_by('-pk')
     content = {'posts' : posts,
                }
-
     return render(request, 'blog/index.html', content)
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    content = {'post' : post}
+    return render(request, 'blog/single_post_page.html', content)
