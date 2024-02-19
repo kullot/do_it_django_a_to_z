92a2e88bde6c38c9405b106cc5212bdf35b3a39a
wlsdn123 <ness9497@gmail.com>
2024-02-20 00:11:39 +0900

    Post 모델에 file_upload 필드 추가하기

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -1,7 +1,11 @@
 ﻿from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('about_me/', views.about_me),
     path('', views.landing),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
